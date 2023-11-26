46810fef69da978a4cf777b90769a0a2f340aad4
Bianor Araujo <bianor.araujo@gmail.com>
2023-11-26 01:49:03 -0300

    added items on navbar

--- scripts/site.js
+++ scripts/site.js
@@ -1,7 +1,20 @@
 function changeThemeMode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
-    document.getElementById("toogleMode").classList.toggle('bi-sun-fill');
-    document.getElementById("toogleMode").classList.toggle('themeModeIconDark');
+    document.getElementById("toogleMode").classList.toggle("bi-sun-fill");
+    document.getElementById("toogleMode").classList.toggle("themeModeIconDark");
+    navbarDarkMode(element);
+ }
 
+
+ function navbarDarkMode(element) {
+    element.getElementsByClassName("navbar")[0].classList.toggle("dark-mode");
+    element.getElementsByClassName("navbar-brand")[0].classList.toggle("dark-mode");
+    element.getElementsByClassName("navbar-toggler")[0].classList.toggle("dark-mode");
+    
+    var listItem = element.getElementsByClassName("nav-link");
+    for (let i = 0; i < listItem.length; i++) {
+        listItem[i].classList.toggle("dark-mode");
+    }
+    
  }
